f84cc1eac6a7c090f2da344a1b8cfac347d0c55b
ArturPaw <162017662+ArturPaw@users.noreply.github.com>
2024-04-07 09:36:00 +0200

    2222

--- src/SredniaOcenZNazwami.java
+++ src/SredniaOcenZNazwami.java
@@ -5,7 +5,7 @@ public class SredniaOcenZNazwami {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Tablica przechowująca nazwy przedmiotów
+      
         String[] nazwyPrzedmiotow = {
             "Matematyka",
             "Fizyka",
@@ -14,27 +14,27 @@ public class SredniaOcenZNazwami {
             "Język angielski"
         };
 
-        // Pobieranie liczby ocen
+        
         System.out.println("Podaj liczbę ocen: ");
         int liczbaOcen = scanner.nextInt();
 
-        // Tablica do przechowywania ocen
+       
         double[] oceny = new double[liczbaOcen];
 
-        // Pobieranie ocen z poszczególnych przedmiotów
+        
         for (int i = 0; i < liczbaOcen; i++) {
             System.out.println("Podaj ocenę z " + nazwyPrzedmiotow[i] + ": ");
             oceny[i] = scanner.nextDouble();
         }
 
-        // Obliczanie średniej arytmetycznej
+        
         double srednia = 0;
         for (double ocena : oceny) {
             srednia += ocena;
         }
         srednia /= liczbaOcen;
 
-        // Wyświetlanie wyniku
+      
         System.out.println("Średnia arytmetyczna ocen: " + srednia);
     }
 }
